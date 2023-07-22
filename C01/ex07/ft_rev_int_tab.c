7c34b1ca268865dd54c5750a270b38375a786541
Mr Norkum <kelestayyib@gmail.com>
2023-07-22 03:37:35 +0300

    Successful

--- C01/ex07/ft_rev_int_tab.c
+++ C01/ex07/ft_rev_int_tab.c
@@ -10,16 +10,18 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_swap(int *a, int *b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+
 void	ft_rev_int_tab(int *tab, int size)
 {
-	int	i;	
-	int	swap;
+	int	i;
 
 	i = -1;
 	while (++i < (size / 2))
-	{
-		swap = tab[i];
-		tab[i] = tab[size - 1 - i];
-		tab[size - 1 - i] = swap;
-	}
+		ft_swap(&tab[i], &tab[size - 1 - i]);
 }
--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -10,23 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 
+void	ft_swap(int *a, int *b)
+{
+	*a = *a + *b;
+	*b = *a - *b;
+	*a = *a - *b;
+}
+
 void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
-	int	swap;
 
 	while (size >= 0)
 	{
 		i = -1;
 		while (++i < size - 1)
-		{
 			if (tab[i] > tab[i + 1])
-			{
-				swap = tab[i];
-				tab[i] = tab[i + 1];
-				tab[i + 1] = swap;
-			}
-		}
+				ft_swap(&tab[i], &tab[i + 1]);
 		size--;
 	}
 }
