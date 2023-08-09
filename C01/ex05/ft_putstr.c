0dee9c21e9d1c894b9353c427a0d619eb150ac1f
Mr. Norkum <kelestayyib@gmail.com>
2023-08-09 15:59:53 +0300

    Successful

--- C01/ex05/ft_putstr.c
+++ C01/ex05/ft_putstr.c
@@ -14,6 +14,14 @@
 
 void	ft_putstr(char *str)
 {
+	while (*str && write(1, str++, 1))
+		;
+}
+
+void	ft_putstr(char *str)
+{
 	while (*str)
 		write(1, str++, 1);
 }
+
+//Hangisi kolayınıza geliyorsa onu kullanın.d
