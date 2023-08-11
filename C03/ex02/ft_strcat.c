3c6a30f7a48865d9a508b0dc826a852497905d73
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 16:37:04 +0300

    Successful

--- C03/ex02/ft_strcat.c
+++ C03/ex02/ft_strcat.c
@@ -1,20 +1,12 @@
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = -1;
-	while (str[++i])
-		;
-	return (i);
-}
-
 char	*ft_strcat(char *dest, char *src)
 {
 	int	d;
 	int	i;
 
 	i = -1;
-	d = ft_strlen(dest);
+	d = 0;
+	while (dest[d])
+	    d++;
 	while (src[++i])
 		dest[d + i] = src[i];
 	dest[d + i] = '\0';
