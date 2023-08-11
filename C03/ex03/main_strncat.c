1f3a3dd571973a8f2f548ead922100f1cf4a8ac6
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 16:41:36 +0300

    Successful

--- C03/ex03/main_strncat.c
+++ C03/ex03/main_strncat.c
@@ -1,33 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strncat.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/08 12:48:26 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/12 07:27:23 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = -1;
-	while (str[++i])
-		;
-	return (i);
-}
-
 char	*ft_strncat(char *dest, char *src, unsigned int nb)
 {
 	unsigned int	d;
 	unsigned int	i;
 
 	i = -1;
-	d = ft_strlen(dest);
-	while (src[++i] != '\0' && i < nb)
+	d = 0;
+	while (dest[d])
+		d++;
+	while (src[++i] && i < nb)
 		dest[d + i] = src[i];
 	dest[d + i] = '\0';
 	return (dest);
@@ -36,7 +16,7 @@ char	*ft_strncat(char *dest, char *src, unsigned int nb)
 #include <stdio.h>
 int main(void)
 {
-	char dest[27] = "Mr. Norkum ";
-	char src[] = "Was Here Xd";
-	printf("%d", ft_strncat(dest, src, 9));
+	char dest[27] = "Mr. Norkum";
+	char src[] = " Was Here Xd";
+	printf("%s", ft_strncat(dest, src, 9));
 }
