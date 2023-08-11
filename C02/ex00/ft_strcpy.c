d0296d1ac4c38c6f160af61f2e6f7ef5fd7ec5e7
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 22:32:09 +0300

    Successful

--- C02/ex00/ft_strcpy.c
+++ C02/ex00/ft_strcpy.c
@@ -5,6 +5,5 @@ char    *ft_strcpy(char *dest, char *src)
     i = -1;
     while (src[++i])
         dest[i] = src[i];
-    dest[i] = '\0';
-    return (dest);
+    return (dest[i] = '\0', dest);
 }
