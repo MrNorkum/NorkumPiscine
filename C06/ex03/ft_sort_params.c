8a108dc25e6528c4b6ffde40a647628f4d8b9461
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 19:02:12 +0300

    Successful

--- C06/ex03/ft_sort_params.c
+++ C06/ex03/ft_sort_params.c
@@ -1,36 +1,17 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_sort_params.c                                   :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/10 17:02:58 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/14 03:44:06 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
-void	ft_putstr(char *str)
+void	ft_putendl(char *str)
 {
-	while (*str)
-		write(1, str++, 1);
+	while (*str && write(1, str++, 1))
+		;
+	write(1, "\n", 1);
 }
 
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	i;
-
-	i = 0;
-	while (s1[i] || s2[i])
-	{
-		if (s1[i] == s2[i])
-			i++;
-		else
-			return (s1[i] - s2[i]);
-	}
-	return (0);
+	while (*s1 && *s2 && *s1++ == *s2++)
+		;
+	return (*--s1 - *--s2);
 }
 
 void	ft_swap(char **a, char **b)
@@ -52,10 +33,8 @@ int	main(int ac, char **av)
 	{
 		a = 0;
 		while (++a < ac - 1)
-		{
 			if (ft_strcmp(av[a], av[a + 1]) > 0)
 				ft_swap(&av[a], &av[a + 1]);
-		}
 	}
 	a = 0;
 	while (++a < ac)
@@ -63,5 +42,4 @@ int	main(int ac, char **av)
 		ft_putstr(av[a]);
 		write(1, "\n", 1);
 	}
-	return (0);
 }
