dd397747355bba2e56fc21b5f21b36bd72f07ca4
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 17:14:23 +0300

    Successful

--- C05/ex01/ft_recursive_factorial.c
+++ C05/ex01/ft_recursive_factorial.c
@@ -1,22 +1,8 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_recursive_factorial.c                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/09 22:08:03 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/12 07:50:51 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 int	ft_recursive_factorial(int nb)
 {
-	if (nb < 0)
-		return (0);
-	else if (nb == 0)
+	if (nb == 0 || nb == 1)
 		return (1);
-	else if (0 < nb && nb < 13)
-		return (nb * ft_recursive_factorial(nb -1));
+	else if (2 <= nb && nb <= 12)
+		return (nb * ft_recursive_factorial(nb - 1));
 	return (0);
 }
