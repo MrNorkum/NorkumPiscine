5c340466baa3857c9b7cd42309e95a879650095d
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 16:27:30 +0300

    Successful

--- C03/ex01/ft_strncmp.c
+++ C03/ex01/ft_strncmp.c
@@ -5,7 +5,7 @@ int	ft_strncmp(char *s1, char *s2, unsigned int n)
 	i = 0;
 	if (n == 0)
 		return (0);
-	while (s1[i] && s2[i] && (s1[i] == s2[i]) && i < n - 1)
+	while (s1[i] && s2[i] && s1[i] == s2[i] && i < n - 1)
 		i++;
 	return (s1[i] - s2[i]);
 }
