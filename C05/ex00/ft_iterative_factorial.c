6d93f0f3914e164fe10ed9dbb498973027f7825d
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 17:08:31 +0300

    Successful

--- C05/ex00/ft_iterative_factorial.c
+++ C05/ex00/ft_iterative_factorial.c
@@ -1,27 +1,13 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_iterative_factorial.c                           :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/09 21:20:05 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/12 07:50:32 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 int	ft_iterative_factorial(int nb)
 {
 	int	x;
 	int	i;
 
 	x = 1;
-	i = 0;
-	if (nb < 0)
-		return (0);
-	else if (nb == 0)
+	i = 1;
+	if (nb == 0 || nb == 1)
 		return (1);
-	while (0 < nb && nb < 13)
+	while (2 <= nb && nb <= 12)
 	{
 		while (++i <= nb)
 			x *= i;
