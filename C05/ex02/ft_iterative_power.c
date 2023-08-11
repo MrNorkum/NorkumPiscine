c2dbc0d9d61fb92037e37f9a1e7d711dbfc225aa
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 17:29:00 +0300

    Successful

--- C05/ex02/ft_iterative_power.c
+++ C05/ex02/ft_iterative_power.c
@@ -1,34 +1,19 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_iterative_power.c                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/09 23:28:57 by hkeles            #+#    #+#             */
-/*   Updated: 2023/04/10 16:17:49 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 int	ft_iterative_power(int nb, int power)
 {
-	int	i;
-	int	r;
+	int r;
 
-	i = 1;
 	r = 1;
-	if (nb == 0 && power == 0)
+	if (power < 0)
+		return (0);
+	if (power == 0)
 		return (1);
-	else if (nb == 0 || power < 0)
-		return (0);
-	while (power > 0)
-	{
-		if ((power % 2) == 1)
-			r *= nb;
-		nb *= nb;
-		power /= 2;
-	}
-	if (r > 2147483647)
-		return (0);
+	while (power--)
+	    r *= nb;
 	return (r);
+}
+
+#include <stdio.h>
+int main(void)
+{
+	printf("%d", ft_iterative_power(2, 7));
 }
