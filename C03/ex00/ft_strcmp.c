d3ff5a169183ffb341b43e44c887399c7d5e87d4
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 16:15:39 +0300

    Successful

--- C03/ex00/ft_strcmp.c
+++ C03/ex00/ft_strcmp.c
@@ -1,9 +1,6 @@
 int	ft_strcmp(char *s1, char *s2)
 {
-	int	i;
-
-	i = 0;
-	while (s1[i] && s2[i] && s1[i] == s2[i])
-		i++;
-	return (s1[i] - s2[i]);
+	while (*s1 && *s2 && *s1++ == *s2++)
+		;
+	return (*--s1 - *--s2);
 }
