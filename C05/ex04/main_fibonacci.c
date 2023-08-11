195fe4987e5294cd48a1ab5cdcc219e2b7eba5ac
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 17:44:53 +0300

    Successful

--- C05/ex04/main_fibonacci.c
+++ C05/ex04/main_fibonacci.c
@@ -1,29 +1,14 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_fibonacci.c                                     :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/10 10:00:57 by hkeles            #+#    #+#             */
-/*   Updated: 2023/04/10 16:19:09 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 int	ft_fibonacci(int index)
 {
 	if (index < 0)
 		return (-1);
-	else if (index <= 1)
+	if (index <= 1)
 		return (index);
-	else if (index > 46)
-		return (0);
-	else
-		return (ft_fibonacci(index -1) + ft_fibonacci(index -2));
+	return (ft_fibonacci(index -1) + ft_fibonacci(index -2));
 }
 
 #include <stdio.h>
 int main(void)
 {
-	printf("%d", ft_fibonacci(5));
+	printf("%d", ft_fibonacci(7));
 }
