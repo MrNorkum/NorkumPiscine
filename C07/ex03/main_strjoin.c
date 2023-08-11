9328ce575dd138fc80e4e87f8311ae8ba3f5c4e1
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 20:14:51 +0300

    Successful

--- C07/ex03/main_strjoin.c
+++ C07/ex03/main_strjoin.c
@@ -1,52 +1,32 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/14 06:20:23 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/12 08:41:14 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-#include <stdlib.h>
-
-char	*ft_strjoin(int size, char **strs, char *sep)
+char    *ft_strjoin(int size, char **strs, char *sep)
 {
-	char	*s;
-	int		i;
-	int		j;
-	int		c;
+    char    *s;
+    int        arg;
+    int        index;
+    int        i;
 
-	c = 0;
-	i = -1;
-	s = malloc(sizeof(strs));
-	while (++i < size)
-	{
-		j = 0;
-		while (strs[i][j])
-			s[c++] = strs[i][j++];
-		j = 0;
-		while (sep[j] && i < size - 1)
-			s[c++] = sep[j++];
-	}
-	s[c] = '\0';
-	return (s);
+    i = 0;
+    arg = -1;
+    s = malloc(8000);
+    while (++arg < size)
+    {
+        index = 0;
+        while (strs[arg][index])
+            s[i++] = strs[arg][index++];
+        index = 0;
+        while (sep[index] && arg < size - 1)
+            s[i++] = sep[index++];
+    }
+    s[i] = '\0';
+    return (s);
 }
 
-int	main(void)
+int    main(void)
 {
-	char	**strs;
-	char	*sep;
-	char	*result;
-
-	strs = (char **)malloc(3 * sizeof(char *));
-	strs[0] = (char *)malloc(sizeof(char) * 2 + 1);
-	strs[1] = (char *)malloc(sizeof(char) * 6 + 1);
-	strs[0] = "Mr";
-	strs[1] = "Norkum";
-	sep = " ";
-	result = ft_strjoin(2, strs, sep);
-	printf("%s\n", result);
+    char    **strs = malloc(sizeof(char *) * 10);
+    strs[0] = "Mr";
+    strs[1] = "Norkum";
+    char    sep[] = " ";
+    char    *res = ft_strjoin(2, strs, sep);
+    printf("%s\n", res);
 }
