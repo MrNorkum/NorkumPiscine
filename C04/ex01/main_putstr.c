9d8afe630a7b1aff09879193dc789fdd2c62a1cb
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 16:54:17 +0300

    Successful

--- C04/ex01/main_putstr.c
+++ C04/ex01/main_putstr.c
@@ -1,25 +1,12 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_putstr.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/09 00:16:00 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/12 07:39:49 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include <unistd.h>
 
 void	ft_putstr(char *str)
 {
-	while (*str)
-		write(1, str++, 1);
+	while (*str && write(1, str++, 1))
+		;
 }
 
-#include <stdio.h>
 int main(void)
 {
-	printf("%s", ft_putstr("Mr. Norkum"));
+	ft_putstr("Mr. Norkum");
 }
