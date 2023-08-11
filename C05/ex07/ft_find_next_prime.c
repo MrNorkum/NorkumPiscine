a74d948ce431e6d3ad11b82b6cdca9a37bc23c0d
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 18:23:58 +0300

    Successful

--- C05/ex07/ft_find_next_prime.c
+++ C05/ex07/ft_find_next_prime.c
@@ -1,23 +1,11 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_find_next_prime.c                               :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: hkeles <hkeles@student.42kocaeli.com.tr>   +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/10 15:13:25 by hkeles            #+#    #+#             */
-/*   Updated: 2023/07/12 08:12:18 by hkeles           ###   ########.tr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 int	ft_is_prime(int nb)
 {
-	int	i;
+	long	i;
 
 	i = 2;
 	if (nb <= 1)
 		return (0);
-	while (i <= (nb / 2))
+	while (i * i <= nb)
 	{
 		if (nb % i == 0)
 			return (0);
@@ -30,9 +18,9 @@ int	ft_find_next_prime(int nb)
 {
 	if (nb <= 2)
 		return (2);
-	while (1)
+	while (nb > 2)
 	{
-		if (ft_is_prime(nb) == 1)
+		if (ft_is_prime(nb))
 			return (nb);
 		nb++;
 	}
