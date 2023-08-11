3364f5ce8a33ba8864cb48093996c71cb3ea7971
Mr. Norkum <kelestayyib@gmail.com>
2023-08-11 22:48:24 +0300

    Successful

--- C07/ex02/ft_ultimate_range.c
+++ C07/ex02/ft_ultimate_range.c
@@ -5,16 +5,10 @@ int	ft_ultimate_range(int **range, int min, int max)
 	int		i;
 
 	if (min >= max)
-	{
-		*range = 0;
-		return (0);
-	}
+		return (*range = 0, 0);
 	range[0] = malloc(sizeof(int) * (max - min));
 	if (!range[0])
-	{
-		*range = 0;
-		return (-1);
-	}
+		return (*range = 0, -1);
 	i = -1;
 	while (++i < max - min)
 		range[0][i] = min + i;
