cef48d675afce776d8744afb7a612adbbd2271f3
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 17:03:12 +0300

    Successful

--- C02/ex03/main_str_is_numeric.c
+++ C02/ex03/main_str_is_numeric.c
@@ -1,15 +1,13 @@
 int	ft_str_is_numeric(char *str)
 {
-	if (str[0] == '\0')
-		return (1);
-	while (*str)
-	{
-		if ('0' <= *str && *str <= '9')
-			str++;
-		else
-			return (0);
-	}
-	return (1);
+    while (*str)
+    {
+        if ('0' <= *str && *str <= '9')
+		str++;
+	else
+		return (0);
+    }
+    return (1);
 }
 
 #include <stdio.h>
