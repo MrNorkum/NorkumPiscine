6b4646520b4a27b55b057ff49c4e7f36bc37fd88
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 16:34:39 +0300

    Successful

--- C00/ex06/ft_print_comb2.c
+++ C00/ex06/ft_print_comb2.c
@@ -16,8 +16,8 @@ void	ft_print_comb2(void)
 		b = a + 1;
 		while (b <= 99)
 		{
-			ft_putchar((a / 10) + 48);
-			ft_putchar((a % 10) + 48);
+			ft_putchar((a / 10) + '0');
+			ft_putchar((a % 10) + '0');
 			ft_putchar(' ');
 			ft_putchar((b / 10) + '0');
 			ft_putchar((b % 10) + '0');
