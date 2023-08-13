e0a8c13642d072ca9158c32b138d2805da52353f
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 16:22:43 +0300

    Successful

--- C00/ex02/main_print_reverse_alphabet.c
+++ C00/ex02/main_print_reverse_alphabet.c
@@ -2,25 +2,14 @@
 
 void	ft_print_reverse_alphabet(void)
 {
-	char	x;
+	int	x;
 
-	x = 123;
-	while (--x >= 97)
-		write(1, &x, 1);
+	x = 122;
+	while (x >= 97 && write(1, &x, 1))
+		x--;
 }
 
 int main(void)
 {
 	ft_print_reverse_alphabet();
 }
-
-//Bu defa alfabeyi tersten yazdıracağız.
-//burada gerçekleşen olay şudur.
-//X while koşuluna girmeden önce azalıyor 122 oluyor ve sonra 122 büyük eşit midir 97 sorusunu cevaplıyor.
-//Cevap true ise içeri giriyor ve 122'nin ascii karşılığını ekrana yazdırıyor.
-//İşlem böyle devam eder.
-//En son ise b karakterini yani 98'i yazdırdıktan sonra koşula tekrar girer.
-//Kendini azalttıktan sonra kontrol eder 97 büyük eşit midir 97.
-//Koşulun cevabı true olduğu için içeriye girer ve 97'yi yazdırır.
-//Koşula tekrar girer kendini bir azaltır ve soruyu sorar 96 büyük eşit midir 97?
-//Koşulun cevabı false olduğu için 96 yazdırılmaz ve işlem böylece son bulur.
