9c193a44895cb52bb42a2849e2ab6cf45bfcb685
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 16:11:03 +0300

    Successful

--- C04/ex03/main_atoi.c
+++ C04/ex03/main_atoi.c
@@ -1,19 +1,19 @@
-int my_atoi(const char *str, int sign, int res, int mod)
+int mr_atoi(const char *str, int sign, int res, int mod)
 {
     if (((9 <= *str && *str <= 13) || *str == 32) && res == 0 && mod == 1)
-        return (my_atoi(str + 1, sign, res, mod));
+        return (mr_atoi(str + 1, sign, res, mod));
     if (*str == '-' && res == 0 && mod == 1)
-        return (my_atoi(str + 1, sign * -1, res, mod));
+        return (mr_atoi(str + 1, sign * -1, res, mod));
     if (*str == '+' && res == 0 && mod == 1)
-        return (my_atoi(str + 1, sign, res, mod));
+        return (mr_atoi(str + 1, sign, res, mod));
     if ('0' <= *str && *str <= '9')
-        return (my_atoi(str + 1, sign, res * 10 + *str - 48, 0));
+        return (mr_atoi(str + 1, sign, res * 10 + *str - 48, 0));
     return (res * sign);
 }
 
 int ft_atoi(const char *str)
 {
-    return (my_atoi(str, 1, 0, 1));
+    return (mr_atoi(str, 1, 0, 1));
 }
 
 #include <stdio.h>
