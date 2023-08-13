243e8b2f27f9dee1e53507d27563525bed4131ca
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 16:05:09 +0300

    Successful

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -1,3 +1,5 @@
+//3'ü de aynı işlevi yapar hangisi kolayınıza geliyorsa onu kullanın.d
+----------------------------------------------------------------------
 int	ft_atoi(char *str)
 {
 	int	d;
@@ -17,3 +19,42 @@ int	ft_atoi(char *str)
 		res = res * 10 + *str++ - 48;
 	return (res * d);
 }
+----------------------------------------------------------------------
+int	my_atoi(char *str, int sign, int res)
+{
+	while (*str == 32 || (*str >= 9 && *str <= 13))
+		str++;
+	while (*str == '-' || *str == '+')
+	{
+		if (*str == '-')
+			sign *= -1;
+		str++;
+	}
+	while (*str >= '0' && *str <= '9')
+		res = res * 10 + *str++ - 48;
+	return (res * sign);
+}
+
+int	ft_atoi(char *str)
+{
+	return (my_atoi(str, 1, 0));
+}
+----------------------------------------------------------------------
+int my_atoi(const char *str, int sign, int res, int mod)
+{
+    if (((9 <= *str && *str <= 13) || *str == 32) && res == 0 && mod == 1)
+        return (my_atoi(str + 1, sign, res, mod));
+    if (*str == '-' && res == 0 && mod == 1)
+        return (my_atoi(str + 1, sign * -1, res, mod));
+    if (*str == '+' && res == 0 && mod == 1)
+        return (my_atoi(str + 1, sign, res, mod));
+    if ('0' <= *str && *str <= '9')
+        return (my_atoi(str + 1, sign, res * 10 + *str - 48, 0));
+    return (res * sign);
+}
+
+int ft_atoi(const char *str)
+{
+    return (my_atoi(str, 1, 0, 1));
+}
+----------------------------------------------------------------------
