ef102a06d5504f4822f4e2df4bf73a3721aa95be
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 16:33:35 +0300

    Successful

--- C00/ex05/main_print_comb.c
+++ C00/ex05/main_print_comb.c
@@ -2,23 +2,23 @@
 
 void	ft_print_comb(void)
 {
-	int	a;
-	int	b;
-	int	c;
+	char	a;
+	char	b;
+	char	c;
 
-	a = 48;
-	while (a <= 55)
+	a = '0';
+	while (a <= '7')
 	{
 		b = a + 1;
-		while (b <= 56)
+		while (b <= '8')
 		{
 			c = b + 1;
-			while (c <= 57)
+			while (c <= '9')
 			{
 				write(1, &a, 1);
 				write(1, &b, 1);
 				write(1, &c, 1);
-				if (a != 55)
+				if (a != '7')
 					write(1, ", ", 2);
 				C++;
 			}
@@ -32,11 +32,3 @@ int main(void)
 {
 	ft_print_comb();
 }
-
-//Burada hem kendi içinde hemde her üçlü arasında artan şekilde ilerleyen bir örüntü kurmamız gerekiyor
-//Örneğin 023 035 ya da 478 gibi şeyler kendi içinde artan koşulu sağlarken 120 436 ya da 599 bu koşulu sağlamaz.
-//Çünkü soldaki sayı ne olursa olsun sağındaki en az bir fazlası olmalıdır.
-//aralarında artana ise 018 019 023 gibi bir örnek ya da 078 089 123 gibi bir örnek verilebilir.
-//049 ardından 050 gelemez çünkü 050 kendi içinde artan değildir 056 gelmesi gerekir.
-//Bizde burada bunu yapıyoruz ve üç basamağımız olsduğu için en son yazdıracağımız şey 789 oluyor.
-//if koşılında ise a = 55 yani '7' olmadığı süre boyunca , atması gerektiğini belirtiyoruz.
