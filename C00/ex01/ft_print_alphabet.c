87864c61a6fc02b25f72b7a7bd21ca9099c643a8
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 16:21:11 +0300

    Successful

--- C00/ex01/ft_print_alphabet.c
+++ C00/ex01/ft_print_alphabet.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 
 void	ft_print_alphabet(void)
-
 {
-	char	x;
+	int	x;
 
-	x = 96;
-	while (++x <= 122)
-		write(1, &x, 1);
+	x = 97;
+	while (x <= 122 && write(1, &x, 1))
+		x++;
 }
