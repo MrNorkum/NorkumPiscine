9a81817de32b6775f6c3361946b1e63b47e1661d
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 16:35:31 +0300

    Successful

--- C00/ex06/main_print_comb2.c
+++ C00/ex06/main_print_comb2.c
@@ -16,11 +16,11 @@ void	ft_print_comb2(void)
 		b = a + 1;
 		while (b <= 99)
 		{
-			ft_putchar((a / 10) + 48);
-			ft_putchar((a % 10) + 48);
-			ft_putchar(' ');
-			ft_putchar((b / 10) + '0');
-			ft_putchar((b % 10) + '0');
+			ft_putchar(a / 10 + 48);
+			ft_putchar(a % 10 + 48);
+			ft_putchar(32);
+			ft_putchar(b / 10 + 48);
+			ft_putchar(b % 10 + 48);
 			if (a != 98)
 				write(1, ", ", 2);
 			b++;
