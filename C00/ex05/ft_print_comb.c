f469d0d40d625ce9fe765002ee4c8bc37a2629da
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 16:29:52 +0300

    Successful

--- C00/ex05/ft_print_comb.c
+++ C00/ex05/ft_print_comb.c
@@ -2,23 +2,23 @@
 
 void	ft_print_comb(void)
 {
-	int	a;
-	int	b;
-	int	c;
+	char	a;
+	char	b;
+	char	c;
 
-	a = 48;
-	while (a <= 55)
+	a = '0';
+	while (a <= '7')
 	{
 		b = a + 1;
-		while (b <= 56)
+		while (b <= '8')
 		{
 			c = b + 1;
-			while (c <= 57)
+			while (c <= '9')
 			{
 				write(1, &a, 1);
 				write(1, &b, 1);
 				write(1, &c, 1);
-				if (a != 55)
+				if (a != '7')
 					write(1, ", ", 2);
 				C++;
 			}
