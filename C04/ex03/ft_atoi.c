838c54b7de45222a2f3a252c68115a3319a37954
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 16:10:36 +0300

    Successful

--- C04/ex03/ft_atoi.c
+++ C04/ex03/ft_atoi.c
@@ -20,7 +20,7 @@ int	ft_atoi(char *str)
 	return (res * d);
 }
 ----------------------------------------------------------------------
-int	my_atoi(char *str, int sign, int res)
+int	mr_atoi(char *str, int sign, int res)
 {
 	while (*str == 32 || (*str >= 9 && *str <= 13))
 		str++;
@@ -37,24 +37,24 @@ int	my_atoi(char *str, int sign, int res)
 
 int	ft_atoi(char *str)
 {
-	return (my_atoi(str, 1, 0));
+	return (mr_atoi(str, 1, 0));
 }
 ----------------------------------------------------------------------
-int my_atoi(const char *str, int sign, int res, int mod)
+int mr_atoi(const char *str, int sign, int res, int mod)
 {
     if (((9 <= *str && *str <= 13) || *str == 32) && res == 0 && mod == 1)
-        return (my_atoi(str + 1, sign, res, mod));
+        return (mr_atoi(str + 1, sign, res, mod));
     if (*str == '-' && res == 0 && mod == 1)
-        return (my_atoi(str + 1, sign * -1, res, mod));
+        return (mr_atoi(str + 1, sign * -1, res, mod));
     if (*str == '+' && res == 0 && mod == 1)
-        return (my_atoi(str + 1, sign, res, mod));
+        return (mr_atoi(str + 1, sign, res, mod));
     if ('0' <= *str && *str <= '9')
-        return (my_atoi(str + 1, sign, res * 10 + *str - 48, 0));
+        return (mr_atoi(str + 1, sign, res * 10 + *str - 48, 0));
     return (res * sign);
 }
 
 int ft_atoi(const char *str)
 {
-    return (my_atoi(str, 1, 0, 1));
+    return (mr_atoi(str, 1, 0, 1));
 }
 ----------------------------------------------------------------------
