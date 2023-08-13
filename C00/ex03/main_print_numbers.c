23032766c11da3ca01072c2d54a5b448c9aea21a
Mr. Norkum <kelestayyib@gmail.com>
2023-08-13 16:25:32 +0300

    Successful

--- C00/ex03/main_print_numbers.c
+++ C00/ex03/main_print_numbers.c
@@ -2,24 +2,14 @@
 
 void	ft_print_numbers(void)
 {
-	char	x;
+	int	x;
 
-	x = 47;
-	while (x++ < 57)
-		write(1, &x, 1);
+	x = 48;
+	while (x <= 57 && write(1, &x, 1))
+		x++;
 }
 
 int main(void)
 {
 	ft_print_numbers();
 }
-
-//Bu defa rakamları yazdıracağız.
-//burada gerçekleşen olay şudur.
-//X while koşuluna 47 olarak girer ve 47 küçük müdür 57 sorusunu cevapladıktan sonra kendini bir arttırır.
-//Böylece yazdırma kısmına 48 olarak gelir ve bu işlem böyle devam eder.
-//En son ise 8 karakterini yani 56'yı yazdırdıktan sonra koşula tekrar girer ve kontrol eder.
-//56 küçük müdür 57? ve ardından kendisini bir arttırır.
-//Koşulun cevabı true olduğu için içeriye girer ve 57'yi yazdırır.
-//Koşula tekrar girer ve soruyu sorar 57 küçük müdür 57? ve ardından kendini bir arttırır.
-//Koşulun cevabı false olduğu için 58 yazdırılmaz ve işlem böylece son bulur.
