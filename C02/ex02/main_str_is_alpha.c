c27f20cc29f3f70f67d671489d8fe0a92065575b
Mr. Norkum <kelestayyib@gmail.com>
2023-08-14 21:03:00 +0300

    Successful

--- C02/ex02/main_str_is_alpha.c
+++ C02/ex02/main_str_is_alpha.c
@@ -1,15 +1,9 @@
 int	ft_str_is_alpha(char *str)
 {
-	if (str[0] == '\0')
-		return (1);
 	while (*str)
-	{
-		if (('A' <= *str && *str <= 'Z')
-			|| ('a' <= *str && *str <= 'z'))
-			str++;
-		else
+		if (!(('A' <= *str && *str <= 'Z')
+			|| ('a' <= *str && *str <= 'z')))
 			return (0);
-	}
 	return (1);
 }
 
