1d1872aa1acc3a39faffc72dcea7cd5211528a38
Mr. Norkum <kelestayyib@gmail.com>
2023-08-14 20:57:46 +0300

    Successful

--- C02/ex03/ft_str_is_numeric.c
+++ C02/ex03/ft_str_is_numeric.c
@@ -1,11 +1,7 @@
 int	ft_str_is_numeric(char *str)
 {
 	while (*str)
-	{
-		if ('0' <= *str && *str <= '9')
-			str++;
-		else
+		if (!('0' <= *str && *str++ <= '9'))
 			return (0);
-	}
 	return (1);
 }
