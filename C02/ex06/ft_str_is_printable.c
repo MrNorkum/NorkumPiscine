c172955adf546c394d05359ca661fbb519cffa54
Mr. Norkum <kelestayyib@gmail.com>
2023-08-14 21:12:29 +0300

    Successful

--- C02/ex06/ft_str_is_printable.c
+++ C02/ex06/ft_str_is_printable.c
@@ -1,11 +1,7 @@
 int	ft_str_is_printable(char *str)
 {
 	while (*str)
-	{
-		if (32 <= *str && *str <= 126)
-			str++;
-		else
+		if (!(32 <= *str && *str++ <= 126))
 			return (0);
-	}
 	return (1);
 }
