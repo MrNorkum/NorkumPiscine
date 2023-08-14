2c39caaba74d98baafa2c613674fa081ce019764
Mr. Norkum <kelestayyib@gmail.com>
2023-08-14 21:30:30 +0300

    Successful

--- C03/ex04/main_strstr.c
+++ C03/ex04/main_strstr.c
@@ -3,18 +3,16 @@ char	*ft_strstr(char *str, char *to_find)
 	int	s;
 	int	t;
 
-	s = 0;
-	t = 0;
+	s = -1;
 	if (to_find[0] == '\0')
 		return (str);
-	while (str[s])
+	while (str[++s])
 	{
+		t = 0;
 		while (to_find[t] == str[s + t] && str[s + t])
 			t++;
 		if (to_find[t] == '\0')
 			return (str + s);
-		s++;
-		t = 0;
 	}
 	return (0);
 }
