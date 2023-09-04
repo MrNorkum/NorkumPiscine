c94f3981b769377ba3e09749c5c8af4986bc7091
Mr. Norkum <kelestayyib@gmail.com>
2023-09-04 03:05:25 +0300

    Successful

--- C01/ex08/main_sort_int_tab.c
+++ C01/ex08/main_sort_int_tab.c
@@ -9,13 +9,12 @@ void	ft_sort_int_tab(int *tab, int size)
 {
 	int	i;
 
-	while (size > 1)
+	while (--size)
 	{
 		i = -1;
 		while (++i < size - 1)
 			if (tab[i] > tab[i + 1])
 				ft_swap(&tab[i], &tab[i + 1]);
-		size--;
 	}
 }
 
