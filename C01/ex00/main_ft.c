a5184d886d38fff12b6083474953b0f08afdbb1b
Mr. Norkum <kelestayyib@gmail.com>
2023-09-04 20:14:45 +0300

    Successful

--- C01/ex00/main_ft.c
+++ C01/ex00/main_ft.c
@@ -9,14 +9,14 @@ int main(void)
 	int x = 13;
 	printf("%d\n", x);
 	ft_ft(&x);
-	printf("%d", x);
+	printf("%d\n", x);
 }
 
 //Burada bir integer x değeri oluşturduk.
 //Oluşan x değerimiz ram'de rastgele bir adreste oluşuyor.
 //Biz ise bu x değerine 13 verdik yani x'in ram'deki adresine 13 yazdık.
 //Ardından ft_ft fonksiyonuna *nbr işaretçisine &(ampersan) ile x değişkeninin adresini gönderdik.
-//Böylece artık *nbr işaretçisi artık x değişkeninin ram'deki adresini işaret ediyor.
+//Böylece artık *nbr işaretçisi x değişkeninin ram'deki adresini işaret ediyor.
 //ft_ft fonksiyonunun içinde ise biz bu *nbr işaretçisinin işaret ettiği yere 42 yazıyoruz.
 //Tabiki adres olarak x'in adresini gönderdiğimiz için *nbr işaretçisinin işaret ettiği yer x değişkeninin adresi oluyor.
 //Ve ardından o adrese 42 yazıyoruz.
