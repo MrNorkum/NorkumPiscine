fad51a6919b4d0c3e85fb6a479261cd1def498c1
Mr. Norkum <kelestayyib@gmail.com>
2023-09-23 07:55:30 +0300

    Successful

--- C01/ex08/ft_sort_int_tab.c
+++ C01/ex08/ft_sort_int_tab.c
@@ -1,20 +1,25 @@
-void	ft_swap(int *a, int *b)
+int	ft_swap(int *a, int *b)
 {
 	*a = *a + *b;
 	*b = *a - *b;
 	*a = *a - *b;
+	return (1);
 }
 
-void	ft_sort_int_tab(int *tab, int size)
+void	sort_int_tab(int *tab, int size)
 {
-	int	i;
+	int i;
+	int is_swap;
 
-	while (size >= 0)
+	is_swap = 1;
+	while (is_swap)
 	{
 		i = -1;
+		is_swap = 0;
 		while (++i < size - 1)
-			if (tab[i] > tab[i + 1])
-				ft_swap(&tab[i], &tab[i + 1]);
-		size--;
+		{
+			if (tab[i] < tab[i + 1])
+				is_swap = ft_swap(&tab[i], &tab[i + 1]);
+		}
 	}
 }
