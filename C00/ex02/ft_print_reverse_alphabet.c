24f051d62f607e7d97df4d43e0a35c5f7d0a6704
Mr. Norkum <kelestayyib@gmail.com>
2023-09-26 03:21:45 +0300

    Successful

--- C00/ex02/ft_print_reverse_alphabet.c
+++ C00/ex02/ft_print_reverse_alphabet.c
@@ -8,3 +8,10 @@ void	ft_print_reverse_alphabet(void)
 	while (x >= 97 && write(1, &x, 1))
 		x--;
 }
+
+void	ft_print_reverse_alphabet(void)
+{
+	write(1, "zyxwvutsrqponmlkjihgfedcba", 26);
+}
+
+//Hangisini kullanmak isterseniz onu kullanın.d
