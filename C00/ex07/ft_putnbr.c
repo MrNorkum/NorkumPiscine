92d3dc9f91fda9553ccc4bfd7cdcf9a824bd20ac
Mr. Norkum <kelestayyib@gmail.com>
2023-09-26 03:11:40 +0300

    Successful

--- C00/ex07/ft_putnbr.c
+++ C00/ex07/ft_putnbr.c
@@ -14,3 +14,17 @@ void	ft_putnbr(int nb)
 		ft_putnbr(n / 10);
 	write(1, &"0123456789"[n % 10], 1);
 }
+
+void	ft_putnbr(int nb)
+{
+    long    n;
+    
+    n = nb;
+    write(1, "-", (n < 0));
+    n *= !(n < 0) - (n < 0);
+    if (n > 9)
+       ft_putnbr(n / 10);
+    write(1, &"0123456789"[n % 10], 1);
+}
+
+//Hangisi kolayınıza geliyorsa onu kullanın.d
