f1fcfbe7b36c4c6ae6c176c5fd65fa00ee81d177
Mr. Norkum <kelestayyib@gmail.com>
2023-09-26 03:23:47 +0300

    Successful

--- C00/ex03/ft_print_numbers.c
+++ C00/ex03/ft_print_numbers.c
@@ -8,3 +8,10 @@ void	ft_print_numbers(void)
 	while (x <= 57 && write(1, &x, 1))
 		x++;
 }
+
+void	ft_print_numbers(void)
+{
+	write(1, "0123456789", 10);
+}
+
+//Hangisini kullanmak isterseniz onu kullanın.d
