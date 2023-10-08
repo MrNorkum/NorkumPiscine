d0b35ad4a9cf63efab2c5fc9ae31ef94376ba081
Mr. Norkum <kelestayyib@gmail.com>
2023-10-08 22:17:47 +0300

    S

--- C05/ex02/main_iterative_power.c
+++ C05/ex02/main_iterative_power.c
@@ -2,12 +2,8 @@ int	ft_iterative_power(int nb, int power)
 {
 	int r;
 
-	r = 1;
-	if (power < 0)
-		return (0);
-	if (!power)
-		return (1);
-	while (power--)
+	r = (power >= 0);
+	while (r && power--)
 	    r *= nb;
 	return (r);
 }
