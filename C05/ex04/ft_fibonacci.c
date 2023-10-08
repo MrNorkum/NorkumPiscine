d0875712593e1c705e69527f4b291ce771e52d51
Mr. Norkum <kelestayyib@gmail.com>
2023-10-08 22:34:01 +0300

    Successful

--- C05/ex04/ft_fibonacci.c
+++ C05/ex04/ft_fibonacci.c
@@ -6,3 +6,12 @@ int	ft_fibonacci(int index)
 		return (index);
 	return (ft_fibonacci(index -1) + ft_fibonacci(index -2));
 }
+
+int	ft_fibonacci(int index)
+{
+	if (index <= 1)
+		return (index * (index >= 0) - (index < 0));
+	return (ft_fibonacci(index - 1) + ft_fibonacci(index - 2));
+}
+
+//Hangisi kolayınıza geliyorsa onu kullanın.d
