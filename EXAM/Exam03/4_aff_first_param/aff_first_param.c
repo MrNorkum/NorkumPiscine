0a50747429f5d58dfc4e59a56db48269b06c9755
Mr. Norkum <kelestayyib@gmail.com>
2024-02-05 02:10:20 +0300

    Successful

--- EXAM/Exam03/4_aff_first_param/aff_first_param.c
+++ EXAM/Exam03/4_aff_first_param/aff_first_param.c
@@ -1,7 +1,7 @@
 #include <unistd.h>
 int	main(int ac, char **av)
 {
-	if (ac-- > 1)
-		while (*av[ac] && write(1, av[ac]++, 1));
+	if (ac > 1)
+		while(*av[1] && write(1, av[1]++, 1));
 	write(1, "\n", 1);
 }
