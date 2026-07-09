8e1e5cde7ea36172d0bfa2cab115150e8aa779d0
Mr. Norkum <78109294+MrNorkum@users.noreply.github.com>
2026-07-09 11:33:16 +0300

    Add pushbot alias to .bashrc and .zshrc

--- PushBot/install.sh
+++ PushBot/install.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
 cp pushbot_v2.3.sh ~/pushbot_v2.3.sh
+echo 'alias pushbot="bash ~/pushbot_v2.3.sh"' >> ~/.zshrc
+echo 'alias pushbot="bash ~/pushbot_v2.3.sh"' >> ~/.bashrc
 alias pushbot="bash ~/pushbot_v2.3.sh"
