4523cec6d47e73a01880bdd34bf9c969dad706ee
Mr. Norkum <78109294+MrNorkum@users.noreply.github.com>
2026-07-09 11:28:13 +0300

    Remove comments from ft_print_program_name.c
    Removed detailed comments explaining the main function and its parameters.

--- C06/ex00/ft_print_program_name.c
+++ C06/ex00/ft_print_program_name.c
@@ -28,39 +28,3 @@ int
 	return (!write(1, argv[0], i));
 }
 
-/*
-** main:
-**   Parametreler:
-**     - int argc: Komut satırından gelen argüman sayısı.
-**     - char *argv[]: Komut satırından gelen argüman dizisi.
-**   İşlev:
-**     - argv[0] karakter dizisini ekrana yazdırır.
-**     - Her bir karakter yazdırıldıktan sonra argv[0] işaretçisi bir sonraki karaktere ilerletilir.
-**     - argc'nin değeri 0 değil ise program başarılı bir şekilde tamamlanmış demektir, bu durumda 0 döndürülür.
-**
-** Açıklamalar:
-**   Bu dosya, main fonksiyonu ile komut satırından gelen ilk argümanın karakter dizisini ekrana yazdırır.
-**
-** 1. main fonksiyonu:
-**    - Parametreler:
-**      - int argc: Komut satırından gelen argüman sayısı.
-**      - char *argv[]: Komut satırından gelen argüman dizisi.
-**    - İşlev:
-**      - argv[0] karakter dizisi üzerinde döngü başlatılır.
-**      - *argv[0] ifadesi ile argv[0] işaretçisinin gösterdiği karakter dizisinin başlangıç adresi alınır.
-**      - write fonksiyonu kullanılarak argv[0] işaretçisi gösterdiği her bir karakter ekrana yazdırılır.
-**      - argv[0] işaretçisi bir sonraki karaktere ilerletilir (++argv[0]).
-**      - argc'nin değeri 0 ise, yani hiç argüman verilmemişse, return (!argc); ifadesi ile 0 döndürülür (başarılı tamamlama).
-**
-** 2. write fonksiyonu:
-**    - Parametreler:
-**      - 1: Standart çıkış.
-**      - argv[0]: Ekrana yazdırılacak karakter dizisi.
-**      - 1: Yazdırılacak karakter dizisinin uzunluğu (1 karakter).
-**    - İşlev:
-**      - Verilen karakter dizisini ekrana yazar.
-**
-** 3. return (!argc);
-**    - İşlev:
-**      - argc'nin değeri 0 değil ise 0 döndürür (başarılı tamamlama).
-*/
